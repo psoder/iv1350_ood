95939963aaf506d8be12b9f4b198141de185def4
Pontus <52171526+psoder@users.noreply.github.com>
2022-04-24 18:49:04 +0200

    Merge files

--- app/src/main/kotlin/model/Wrappers.kt
+++ app/src/main/kotlin/model/Wrappers.kt
@@ -1,5 +1,7 @@
 package model
 
+import java.time.LocalDateTime
+
 /**
  * VAT rate.of a product/service.
  *
@@ -35,3 +37,26 @@ data class Item(
  * @param quantity is the number of items.
  */
 data class SaleItem(val item: Item, val discount: Int, val quantity: Int){}
+
+/**
+ * DTO responsible for storing information about a sale. Immutable.
+ *
+ * @param items is a list of [SaleItem].
+ * @param amountPaid is how much the customer paid.
+ */
+data class Receipt(val items: List<SaleItem>, val amountPaid: Double) {
+    
+    val time = LocalDateTime.now()
+
+    val price = items.fold(0.0) { acc, (item, disc, qty) ->
+        acc + (item.price)
+            .times((100.0 - disc) / 100.0)
+            .times(qty)
+    }
+
+    val vat = items.fold(0.0) { acc, (item, disc, qty) ->
+        acc + (item.price * item.vat.rate / 100)
+            .times((100.0 - disc) / 100.0)
+            .times(qty)
+    }
+}
