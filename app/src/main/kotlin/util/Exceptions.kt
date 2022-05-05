b69b9f1a2d5a1d34b32b347a11a1f4f9b2b506d8
Pontus <52171526+psoder@users.noreply.github.com>
2022-05-05 13:58:08 +0200

    Add more images and some documentation

--- app/src/main/kotlin/util/Exceptions.kt
+++ app/src/main/kotlin/util/Exceptions.kt
@@ -1,8 +1,10 @@
 package util
 
-import kotlin.Exception
+import java.lang.Exception
 
 /**
- * En exception that is thrown when a service isn't available.
+ * An exception that is thrown when a service isn't available.
+ * 
+ * @param message is the exception message.
  */
 class NoSuchServiceException(message: String) : Exception(message) {}
